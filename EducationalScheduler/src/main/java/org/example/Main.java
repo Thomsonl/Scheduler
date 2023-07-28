84e0e1d251b33f7cd33e69fbc6c10e3e98e6e4d2
Thomsonl <thomsonklee@gmail.com>
2023-07-27 22:06:41 -0400

    Sync

--- EducationalScheduler/src/main/java/org/example/Main.java
+++ EducationalScheduler/src/main/java/org/example/Main.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
